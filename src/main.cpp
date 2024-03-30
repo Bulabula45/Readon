6a105d45f0f6d690ff79c9005678b59fa5ce249e
Siang Chen <129380598+Bulabula45@users.noreply.github.com>
2024-03-30 17:03:17 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -13,31 +13,49 @@ int main(int argc, char *argv[])
     const char *output_file = nullptr;
     const char *mode = "all";
     const char *helpMessage = R"(
-Usage: <program_name> -i <reads_file> -o <output_file> [-m <mode>]
--m <mode> : Specify the mode ('error' to select error mode with sequencing data errors; otherwise,
-a larger k-mer size will be used by default)
+Usage: <program_name> -i <reads_file> -o <output_file> [-m <mode>] [-k <kmer>] [-s <step>] [-w <window_size>] [-h]
+-m <mode> : Specify the mode ('error' to select error mode with sequencing data errors, kmer=15; otherwise, a larger k-mer size(25) will be used by default)
+-k <kmer> : Specify the k-mer size (default is 25), maximum is 32
+-s <step> : Specify the interval value (default is 10. If error mode is error, the default value is 6)
+-w <window_size> : Specify the window_s value (default is 15)
+-h : Display this help message
 )";
 
     int opt;
-    while ((opt = getopt(argc, argv, "i:o:m:")) != -1)
+    kmer_l = 25, kmer_2l = kmer_l * 2;
+    interval = 10;
+    window_s = kmer_l - interval;
+    while ((opt = getopt(argc, argv, "i:o:m:k:s:w:h?")) != -1)
     {
         switch (opt)
         {
-        case 'i':
-            reads_file = optarg;
-            break;
-        case 'o':
-            output_file = optarg;
-            break;
-        case 'm':
-            if (optarg[0] != '\0')
-            {
-                mode = optarg;
-            }
-            break;
-        default:
-            std::cerr << helpMessage << endl;
-            return 1;
+            case 'i':
+                reads_file = optarg;
+                break;
+            case 'o':
+                output_file = optarg;
+                break;
+            case 'm':
+                if (optarg[0] != '\0')
+                    mode = optarg;
+                break;
+            case 'k':
+                kmer_l = atoi(optarg);
+                kmer_2l = kmer_l * 2;
+                break;
+            case 's':
+                interval = atoi(optarg);
+                break;
+            case 'w':
+                window_s = atoi(optarg);
+                break;
+            case 'h': // Help option
+            case '?': // Invalid option
+                std::cout << helpMessage << std::endl;
+                return 0;
+            default:
+                std::cerr << helpMessage << std::endl;
+                return 1;
         }
     }
 
@@ -49,17 +67,11 @@ a larger k-mer size will be used by default)
 
     if (strcmp(mode, "e") == 0)
     {
-        kmer_l = 15;
-        interval = 6;
-        kmer_2l = kmer_l * 2;
-        window_s = kmer_l - interval;
-    }
-    else
-    {
-        kmer_l = 25;
-        interval = 10;
-        kmer_2l = kmer_l * 2;
-        window_s = kmer_l - interval;
+        {
+            kmer_l = 15, kmer_2l = kmer_l * 2;
+            interval = 6;
+            window_s = kmer_l - interval;
+        }
     }
 
     initializeConstants();
@@ -70,7 +82,7 @@ a larger k-mer size will be used by default)
     const char* output_fa = output_fa_str.c_str();
 
     cout << "kmer length: " << kmer_l << endl;
-    cout << "window size: " << window_s << endl;
+    cout << "window size: " << window_s << " kmers" << endl;
 
     int reads_fd = open(reads_file, O_RDONLY);
     off_t reads_len = lseek(reads_fd, 0, SEEK_END);
@@ -106,12 +118,8 @@ a larger k-mer size will be used by default)
         mapping(last_reads_per, linesPerRecord, reads_len - offset, offset, reads_fd, outputTsv, outputReads);
         close(reads_fd);
 
-    
-    
     }
 
-
-    
 
     return 0;
 }
